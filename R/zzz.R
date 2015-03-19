ec527f204456cb281c1d312585f27f872a644df9
Seth,Sahil <sseth@mdanderson.org>
2015-03-19 16:02:50 -0500

    update .onAttach
    Remove source bashrc. Need to find a better to check PATH variable
    across platforms.

--- R/zzz.R
+++ R/zzz.R
@@ -6,17 +6,19 @@
     packageStartupMessage("Adding flowr executable to ~/bin")
     file.symlink(system.file(package = pkg, "scripts/flowr"), "~/bin/flowr")
   }
-  if(!file.exists(system("source ~/.bashrc;which flowr", intern = TRUE))){
-    packageStartupMessage("Consider adding ~/bin to your PATH variable in bashrc.",
-                          "This would streamline invoking flowr commands from the shell.",
-                          "\nexport PATH=$PATH:$HOME/bin",
-                          "If you have already added this and still see this message, run 'flowr' from shell and see if it works.",
-                          "You may also add #!/bin/bash as the first line in your .bashrc")
+  if(FALSE){ ## need to find a smarter way to deal with this. i.e. check if PATH variable has flowr
+    if(!file.exists(system("source ~/.bashrc;which flowr", intern = TRUE))){
+      packageStartupMessage("Consider adding ~/bin to your PATH variable in bashrc.",
+                            "This would streamline invoking flowr commands from the shell.",
+                            "\nexport PATH=$PATH:$HOME/bin",
+                            "If you have already added this and still see this message, run 'flowr' from shell and see if it works.",
+                            "You may also add #!/bin/bash as the first line in your .bashrc")
+    }
   }
 }
 
 .onDeatch <- function(lib, pkg) {
-    ops <- options()
-    ops <- ops[grep("SaturnV_", names(ops))]
-    options(ops)
+  ops <- options()
+  ops <- ops[grep("SaturnV_", names(ops))]
+  options(ops)
 }
