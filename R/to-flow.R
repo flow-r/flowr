eec4b74e15c246c99194114a1416e2b6250c9a9c
Sahil Seth <sahiilseth@gmail.com>
2015-07-10 16:32:42 -0500

    update

--- R/to-flow.R
+++ R/to-flow.R
@@ -84,8 +84,11 @@ to_flow.data.frame <- function(x, def,
 	submit = FALSE,
 	execute = FALSE,
 	qobj, ...){
-		if(execute|submit)
+		if(execute|submit){
 			warning("This feature has been depciated. Please to fobj <- to_flow(...); submit_flow(fobj)")
+			if(execute)
+				submit = TRUE
+		}
 		
 		message("\n\n##--- Getting default values for missing parameters...")
 		## --- get defaults sample, job and cmd columns
