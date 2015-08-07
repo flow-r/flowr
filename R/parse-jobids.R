1cfd01d445fbe6d62dcbc83c2c0d6c531b820eb3
Sahil Seth <sahiilseth@gmail.com>
2015-08-07 18:02:31 -0500

    getOption to get_opts

--- R/parse-jobids.R
+++ R/parse-jobids.R
@@ -5,41 +5,41 @@
 #' @param platform string specifying the platform. This determines how the jobids are parsed
 #' @export
 parse_jobids <- function(jobids, platform){
-	
+
 	## --- TORQUE
 	##     Example
-	##  40947.dqsfacpriv01.mdanderson.edu 
+	##  40947.dqsfacpriv01.mdanderson.edu
 	##  parse into: -->>> 40947
 	## if there is a . in the middle, get stuff before it
 	if(platform=="torque")
-		jobids <- gsub(getOption("flow_parse_torque"),"\\1", jobids)
+		jobids <- gsub(get_opts("flow_parse_torque"),"\\1", jobids)
 
 	## --- LSF
-	## --- Example: 
+	## --- Example:
 	## Job <4809> is submitted to queue <transfer>.
 	##  parse into: --->>> 4809
 	if(platform=="lsf")
-		jobids <- gsub(getOption("flow_parse_lsf"),"\\1", jobids)
-	
-	
+		jobids <- gsub(get_opts("flow_parse_lsf"),"\\1", jobids)
+
+
 	## --- moab
 	## --- Example (has empty lines):
 	## parse into: --->>> 97724
 	if(platform == "moab")
-		jobids = gsub(getOption("flow_parse_moab"), "\\1", jobids)
-	
+		jobids = gsub(get_opts("flow_parse_moab"), "\\1", jobids)
+
 	if(platform == "sge")
-		jobids = gsub(getOption("flow_parse_sge"), "\\1", jobids)
+		jobids = gsub(get_opts("flow_parse_sge"), "\\1", jobids)
 
 	if(platform == "slurm")
-		jobids = gsub(getOption("flow_parse_slurm"), "\\1", jobids)
-	
+		jobids = gsub(get_opts("flow_parse_slurm"), "\\1", jobids)
+
 	## ""      "98337"
 	## --- output has multiple rows, split them
 	jobids = na.omit(as.vector(jobids))
 	## remove rows with missing data
 	jobids = jobids[!jobids == ""]
-	
+
 	## --- check how jobids looks
 	##     forcing jobids to be numeric !!
 	##     is this a big assumption ?
--- R/setup.R
+++ R/setup.R
@@ -1,7 +1,7 @@
 #' Setup and initialize some scripts.
-#' 
+#'
 #' @param bin path to bin folder
-#' 
+#'
 #' @details Will add more to this to identify cluster and aid in other things
 #' @export
 setup <- function(bin = "~/bin"){
@@ -15,11 +15,11 @@ setup <- function(bin = "~/bin"){
 	tmp <- c("Consider adding ~/bin to your PATH variable in .bashrc.",
 		"\nexport PATH=$PATH:$HOME/bin",
 		"\nYou may now use all R functions using 'flowr' from shell.")
-	
+
 	message("Creating a directory structure under ",
-		getOption("flow_base_path"))
-	dir.create(getOption("flow_base_path"), showWarnings = FALSE)
-	dir.create(getOption("flow_conf_path"), showWarnings = FALSE)
-	dir.create(getOption("flow_run_path"), showWarnings = FALSE)
+		get_opts("flow_base_path"))
+	dir.create(get_opts("flow_base_path"), showWarnings = FALSE)
+	dir.create(get_opts("flow_conf_path"), showWarnings = FALSE)
+	dir.create(get_opts("flow_run_path"), showWarnings = FALSE)
 	message(tmp)
 }
