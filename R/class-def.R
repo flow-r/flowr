c903bfc3fbfe7f16e71d11927c52bd8393d2b8a3
Sahil Seth <me@sahilseth.com>
2015-08-28 18:30:06 -0500

    better documentation on queue

--- R/class-def.R
+++ R/class-def.R
@@ -93,17 +93,23 @@ setClass("flow", representation(jobs = "list",
 #'
 #'
 #' @details
-#' ## Resources:
+#' \strong{Resources}:
 #' Can be defined **once** using a \link{queue} object and recylced to all the jobs in a flow. If resources (like memory, cpu, walltime, queue) are supplied at the
 #' job level they overwrite the one supplied in \link{queue}
 #' Nodes: can be supplied ot extend a job across multiple nodes. This is purely experimental and not supported.
-#' ## Server:
+#' 
+#' \strong{Server}:
 #' This a hook which may be implemented in future.
-#' ## Submission script:
+#' 
+#' \strong{Submission script}
 #' The 'platform' variable defines the format, and submit_exe; however these two are avaible for someone to create a custom submission command.
+#' 
 #' @inheritParams job
+#' 
 #' @keywords queue
+#' 
 #' @export
+#' 
 #' @examples
 #' qobj <- queue(platform='lsf')
 queue <- function(object,
