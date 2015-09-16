649832bf3058faef7a5be90e6141abd4053e28c3
Sahil Seth <me@sahilseth.com>
2015-09-16 12:43:02 -0500

    important path on check.flowdef

--- R/check.R
+++ R/check.R
@@ -158,8 +158,8 @@ check.flowdef <- function(x, verbose = get_opts("verbose"), ...){
 		if(verbose > 1) 
 			message("\t", i,": ", x$jobname[i], "\t", appendLF = FALSE)
 		prev_jobs = unlist(strsplit(x$prev_jobs[i], ","))
-		p.sub = subset(x, jobname %in% prev_jobs)$sub_type
-		p.dep = subset(x, jobname %in% prev_jobs)$dep_type
+		p.sub = x[x$jobname %in% prev_jobs, 'sub_type']
+		p.dep = x[x$jobname %in% prev_jobs, 'dep_type']
 		check_dep_sub_type(dep = x$dep_type[i],
 											 sub = x$sub_type[i],
 											 p.sub = p.sub,
