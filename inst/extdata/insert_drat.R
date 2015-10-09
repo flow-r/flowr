42596d798d6b3b229bbd9b7bcbf743c53b8e607d
Sahil Seth <me@sahilseth.com>
2015-10-08 19:36:55 -0500

    update travis and remove devtools

--- inst/extdata/insert_drat.R
+++ inst/extdata/insert_drat.R
@@ -8,16 +8,14 @@ pkg = devtools::build("~/Dropbox/public/github_funr")
 pkg = devtools::build("~/Dropbox/public/github_ngsflows")
 pkg = devtools::build("~/Dropbox/public/github_params")
 
-out = drat::insertPackage(pkg, "~/Dropbox/public/github_drat", 
-													c("add", "commit", "push"))
+out = drat::insertPackage(pkg, "~/Dropbox/public/github_drat", TRUE)
+system("cd ~/Dropbox/public/github_drat;git push")
 
 
 
 if(FALSE){
 	## drat for user:
 	#library(drat)
-	system("git commit -a -m 'update website'")
-	system("git push")
 	
 	#drat::addRepo("sahilseth")
 	install.packages("params", repos = "http://sahilseth.github.io/drat")
