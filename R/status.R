c8825a0d7e8bdb30157794e361509c4a5fc4f6e9
Sahil Seth <me@sahilseth.com>
2015-11-24 18:44:09 -0600

    in status, if x is a flow; show status and exit

--- R/status.R
+++ R/status.R
@@ -71,9 +71,11 @@ status <- function(x,
 	if(missing(x))
 		stop("Please provide a path to a flow wd. x='my-awesome-flowpath'")
 	
-	## if a flow object it specified
-	if(is.flow(x))
+	# if a flow object it specified
+	if(is.flow(x)){
 		get_status(x, out_format = out_format, verbose = verbose, use_cache = use_cache, ...)
+	  return()
+	}
 	
 	wds = get_wds(x)
 	lst = lapply(wds, function(wd){
