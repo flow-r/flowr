245b4da7e73ef1d7d7af3f4722952632345223e2
Sahil Seth <me@sahilseth.com>
2015-12-02 12:58:53 -0600

    update docs

--- R/to-flowmat.R
+++ R/to-flowmat.R
@@ -15,21 +15,42 @@
 #' 
 #' @examples 
 #' 
-#' ## Use this link for a few examples:
-#' ## http://docs.flowr.space/flowr/tutorial.html#define_modules
+#' # Use this link for a few examples:
+#' # http://docs.flowr.space/tutorial.html#define_modules
 #' 
-#' ## create a vector of shell commands
-#' cmds = c("sleep 1", "sleep 2")
-#' ## create a named list
-#' lst = list("sleep" = cmds)
-#' ## create a flowmat
+#' # create a flow mat, starting with a list of commands.
+#' cmd_sleep = c("sleep 1", "sleep 2")
+#' cmd_echo = c("echo 'hello'", "echo 'hello'")
+#' 
+#' # create a named list
+#' lst = list("sleep" = cmd_sleep, "echo" = cmd_echo)
 #' flowmat = to_flowmat(lst, samplename = "samp")
 #' 
-#' ## Use flowmat to create a skeleton flowdef
+#' 
+#' 
+#' # read in a tsv; check and confirm format
+#' ex = file.path(system.file(package = "flowr"), "pipelines")
+#' 
+#' flowmat = as.flowmat(file.path(ex, "sleep_pipe.tsv"))
+#' 
+#' # if your column names are different than defaults, explicitly specify them.
+#' flowmat = as.flowmat(file.path(ex, "sleep_pipe.tsv"), jobname_col = "jobname")
+#' 
+#' # check if a object is a flowmat
+#' is.flowmat(flowmat)
+#' 
+#' 
+#' 
+#' # create a flowdef, from this flowmat
 #' flowdef = to_flowdef(flowmat)
 #' 
-#' ## use both (flowmat and flowdef) to create a flow
+#' # create a flow object using flowmat and flowdef
 #' fobj = to_flow(flowmat, flowdef)
+#' 
+#' # extract a flowmat from a flow (here the samplename also contains the name of the flow)
+#' flowmat2 = to_flowmat(fobj)
+#' 
+#' 
 #' 
 #' ## submit the flow to the cluster (execute=TRUE) or do a dry-run (execute=FALSE)
 #' \dontrun{
