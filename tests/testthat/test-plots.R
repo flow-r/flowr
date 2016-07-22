7a7b7ae3ac46192b97094517edd01f50eeb4aea9
Sahil Seth <me@sahilseth.com>
2016-07-22 16:46:22 -0500

    update readme with more examples.

--- tests/testthat/test-plots.R
+++ tests/testthat/test-plots.R
@@ -1,11 +1,11 @@
 require(testthat)
 
 if(FALSE){
-	
-	deffile = system.file("extdata/fastq_haplotyper.def", package = "flowr")
-	
-	flowdef = as.flowdef(deffile)
-	plot_flow(head(flowdef, 4), pdf = TRUE)
-	plot_flow(head(flowdef, 7), pdf = TRUE)
-	plot_flow(flowdef, pdf = TRUE)
+  
+  deffile = system.file("extdata/fastq_haplotyper.def", package = "flowr")
+  
+  flowdef = as.flowdef(deffile)
+  plot_flow(head(flowdef, 4), pdf = TRUE)
+  plot_flow(head(flowdef, 7), pdf = TRUE)
+  plot_flow(flowdef, pdf = TRUE)
 }
